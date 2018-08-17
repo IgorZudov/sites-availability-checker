cf7e79657cc9b84fb56ab68e18acdb699e317e89
Ziv <igor_zudov@mail.ru>
2018-08-17 12:49:02 +0300

    добавил навигацию

--- SitesChecker/src/SitesChecker.App/Program.cs
+++ SitesChecker/src/SitesChecker.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace SitesChecker.App
@@ -12,6 +13,7 @@ namespace SitesChecker.App
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+				.UseStartup<Startup>()
+				.UseContentRoot(Directory.GetCurrentDirectory());
 	}
 }
